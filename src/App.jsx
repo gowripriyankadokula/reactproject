42051a62b3bf592035d7075a85a6a2061bd3a2a6
Gowripriyankadokula <gowripriyankadokula@gmail.com>
2025-06-27 15:39:44 +0530

    completed deployement

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+let api_key=import.meta.env.VITE_API_KEY
+const App = () => {
+  return (
+    <div>
+      <h1>hello everyone</h1>
+    </div>
+  )
+}
+
+export default App
